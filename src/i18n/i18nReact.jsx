f88dc528f350cea23a77aa60362429bd497769fe
Prafull Singh <prafull.singh@tellius.com>
2023-11-21 16:29:54 +0530

    passing props

--- src/i18n/i18nReact.jsx
+++ src/i18n/i18nReact.jsx
@@ -7,15 +7,14 @@ import { getI18nInstance } from './i18Instances';
 export default function withTranslation(Component){
     const i18Instance = getI18nInstance()
     function AppWith18n(props){
-        const [rerender,forceRerender] = useState({});
-
+        const [lang,setLangauge] = useState({});
         useEffect(()=>{
-            i18Instance.on('languageChanged',()=>{
-                forceRerender({})
+            i18Instance.on('languageChanged',(language)=>{
+                setLangauge(language)
             })
-        },[forceRerender])
+        },[setLangauge])
 
-        return <Component {...props} />
+        return <Component {...props} lang={lang} />
     }
 
     return AppWith18n;
